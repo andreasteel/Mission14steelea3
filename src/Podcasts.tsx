4d71b25971b0b2ea1e0704c7a320cd282217ac03
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-04-03 13:03:36 -0600

    Add files via upload

---
+++ src/Podcasts.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PodcastInfo(props: any) {
+  return (
+    <>
+      <div>
+        <div className="text-center">
+          <h1 className="display-4">My Podcasts</h1>
+          <h2>I have a podcast called Baconsale!</h2>
+          <p>
+            To check it out go to the Baconsale website{' '}
+            <a href="https://baconsale.com/">here!!</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PodcastInfo;
